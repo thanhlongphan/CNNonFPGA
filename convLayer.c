3cf4039ae1246015787e9b5d0191fe6c42764ef6
thanhlongphan <32907931+thanhlongphan@users.noreply.github.com>
2017-12-13 23:40:15 +0100

    Update convLayer.c

--- convLayer.c
+++ convLayer.c
@@ -1,5 +1,12 @@
 #include <convLayer.h>
 
-void ConvLayerSetup(int inputHeight, int inputDepth, int inputWidth, int stride, int padding, int filterSize){
+convLayer* ConvLayerSetup(int inputHeight, int inputDepth, int inputWidth, int stride, int padding, int filterSize){
+  convLayer *conv = (convLayer*)malloc(sizeof(convLayer));
+  conv->inputHeight = inputHeight;
+  conv->inputDepth = inputDepth;
+  conv->inputWidth = inputWidth;
+  conv->stride = stride;
+  conv->padding = padding;
+  conv->filterSize = filterSize;
   //TODO
 }
