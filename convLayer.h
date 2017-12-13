46c9d3039f150008ab72f5d88b4db984c003cfad
thanhlongphan <32907931+thanhlongphan@users.noreply.github.com>
2017-12-13 23:13:01 +0100

    Update convLayer.h

--- convLayer.h
+++ convLayer.h
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
 
+typedef struct{
+    int inputHeight;
+    int inputDepth;
+    int inputWidth;
+    int outputHeight;
+    int outputWidth;
+    int filterSize;
+    int stride;
+    int padding;
+    float* bias;
+}convLayer;
+
+void ConvLayerSetup(int inputHeight, int inputDepth, int inputWidth, int stride, int padding, int filterSize);
