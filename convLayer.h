1f13df70dc43ef25f585489450e1eafa6b761798
thanhlongphan <32907931+thanhlongphan@users.noreply.github.com>
2017-12-14 18:12:15 +0100

    Update convLayer.h

--- convLayer.h
+++ convLayer.h
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 #include <assert.h>
-
+default_random_engine generator;
+normal_distribution<float> distribution(0.0, RANDOM_NORMAL_STDDEV);
 typedef struct{
     int inputHeight;
     int inputDepth;
     int inputWidth;
     int outputHeight;
     int outputWidth;
+    int outputDepth;
     int filterSize;
+    float**** filter
     int stride;
     int padding;
     float* bias;
+    
 }convLayer;
 
-convLayer* ConvLayerSetup(int inputHeight, int inputDepth, int inputWidth, int stride, int padding, int filterSize);
+convLayer* ConvLayerSetup(int inputHeight, int inputDepth, int inputWidth, int stride, int padding, int filterSize, int filterCount);
