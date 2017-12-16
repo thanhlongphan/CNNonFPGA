c0cb52672d18997432eb6b94e11785599bac0550
thanhlongphan <32907931+thanhlongphan@users.noreply.github.com>
2017-12-16 12:25:03 +0100

    Update cnn.c

--- cnn.c
+++ cnn.c
@@ -1,1 +1,17 @@
+/*
+* 32x32x3 input
+* 5x5x3 filter
+* 
+*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#DEFINE FILTER_WIDTH 5
+#DEFINE FILTER_HEIGHT 5
+#DEFINE FILTER_DEPTH 5
+#DEFINE INPUT_WIDTH 32
+#DEFINE INPUT_HEIGHT 32
+#DEFINE INPUT_DEPTH 3
