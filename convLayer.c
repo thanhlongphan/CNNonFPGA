14cf34c057bbd7489551a0e336a81c383d74f19f
thanhlongphan <32907931+thanhlongphan@users.noreply.github.com>
2017-12-16 10:22:45 +0100

    Delete convLayer.c

--- convLayer.c
+++
@@ -1,7 +0,0 @@
-#include <convLayer.h>
-
-convLayer* ConvLayerSetup(int inputHeight, int inputDepth, int inputWidth, int stride, int padding, int filterSize, int filterCount){
-  convLayer *conv = (convLayer*)malloc(sizeof(convLayer));
-//TODO
-
-}
