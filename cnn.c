2d783713ea1e854b880251ccc9e0a4bec6b6ae3a
thanhlongphan <32907931+thanhlongphan@users.noreply.github.com>
2017-12-17 14:08:37 +0100

    Update cnn.c

--- cnn.c
+++ cnn.c
@@ -12,11 +12,12 @@ using namespace std;
 #define FILTER_DEPTH 3
 #define FILTER_SIZE 5
 #define DEFAULT_STRIDE 1
+#define DEFAULT_BIAS 0.1
 default_random_engine generator;
 normal_distribution<float> distribution(0.0, RANDOM_NORMAL_STDDEV);
 /*  5x5x3 filter
-*   10x10x3 input images
-*
+*   12x12x3 input images
+*   
 */
 typedef struct{
     int inputHeight;
@@ -31,9 +32,53 @@ typedef struct{
     float*** output;
     int stride;
     int padding;
-    float bias;
+    float* bias;
 }conlayer;
 
+
+typedef struct{
+    int inputDepth;
+    int inputWidth;
+    int inputHeight;
+    int outputDepth;
+    int outputWidth;
+    int outputHeight;
+}reluLayer;
+
+typedef struct{
+    int inputDepth;
+    int inputWidth;
+    int inputHeight;
+    int height;
+    int width;
+    int stride;
+    int padding;
+    int outputDepth;
+    int outputWidth;
+    int outputHeight;
+    int pool;
+}poolLayer;
+
+poolLayer* makePooLayer(conlayer* conv, int pool ){
+    pooLayer* pl = (poolLayer*)malloc(sizeof(poolLayer));
+    pl->width = width;
+    pl->inputDepth = conv->inputDepth;
+    pl->inputWidth = conv->inputWidth;
+    pl->inputHeight = conv->inputHeight;
+    pl->stride = conv->stride;
+    //TODO
+}
+
+reluLayer* makeReluLayer(int inputWidth, int inputHeight, int inputDepth){
+   reluLayer* relu = (reluLayer*)malloc(sizeof(reluLayer));
+   relu->inputDepth = inputDepth;
+   relu->inputWidth = inputWidth;
+   relu->inputHeight = inputHeight;
+   relu->outputDepth = inputDepth;
+   relu->outputHeight = outputHeight; 
+   relu->outputWidth = outputWidth;
+   return relu;  
+}
 
 conlayer* makeConlayer(int inputHeight, int inputWidth, int inputDepth, int padding){
     conlayer *conv = (conlayer*)malloc(sizeof(conlayer));
@@ -48,26 +93,33 @@ conlayer* makeConlayer(int inputHeight, int inputWidth, int inputDepth, int padd
     conv->outputWidth = (conv->inputWidth - conv->filterSize)/conv->stride +1;
     conv->padding = padding;
     //initialize filter random
-    res->filter = (float***)malloc(sizeof(float**)*res->stride+1);
+    conv->filter = (float***)malloc(sizeof(float**)*conv->stride+1);
     for(int i=0; i<conv->filterDepth;i++){
         for(int j=0;j<conv->filterSize;j++){
             for(int k=0;k<conv->filterSize;k++){
-                conv->filter[k][j][i] = distribution(generate)/sqrt(inputDepth*filterWidth*filterWidth);
+                conv->filter[i][j][k] = distribution(generate)/sqrt(inputDepth*filterWidth*filterWidth);
             }
         }
     }
-    //calculate output (feature map )
-
+    //(feature map )
+    
+    conv->output = (float***)malloc(sizeof(float**)*conv->outputDepth);
+    for(int dep=0;dep<outputDepth;dep++){
+        for(int h=0;h<outputHeight;h++){
+            for(int w=0;w<outputWidth;w++){
+                //TODO
+            }
+        }
+    }
 
     //bias
     conv->bias = (float*)calloc(conv->outputDepth, sizeof(float));
     for (int i = 0; i < conv->outputDepth; i++) {
         conv->bias[i] = DEFAULT_BIAS;
     }
-
-
-
 }
+
+
 
 
 
